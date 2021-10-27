d255905c8aa4909fca223abf593b4fc5ca14a30b
João Pedro A <jaaopbr@gmail.com>
2021-10-27 00:58:07 -0300

    ✨ feat: added posts page and styles

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
@@ -19,21 +20,23 @@ interface PostsProps {
 export default function Posts({ posts }: PostsProps) {
     return (
         <>
-        <Head>
-            <title>Posts | Ignews</title>
-        </Head>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
 
-        <main className={styles.container}>
-            <div className={styles.posts}>
-                { posts.map(post => (
-                    <a key={post.slug} href="">
-                        <time>{post.updatedAt}</time>
-                        <strong>{post.title}</strong>
-                        <p>{post.excerpt}</p>
-                    </a>
-                )) }
-            </div>
-        </main>
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    {posts.map(post => (
+                        <Link key={post.slug} href={`/posts/${post.slug}`}>
+                            <a>
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+                        </Link>
+                    ))}
+                </div>
+            </main>
         </>
     );
 }
